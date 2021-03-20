bf18b8a4399a81ff9b0eb55e34a55a17b838f94f
ansy-mir <ansy.mir@gmail.com>
2021-03-20 11:13:26 +0100

    change videoPLayer for second Page.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,20 @@
 import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
+import Form from './modules/form';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const slider = new MainSlider({btns: '.next', container: '.page'});
-  slider.render();
+  const indexSlider = new MainSlider({btns: '.next', container: '.page'});
+  indexSlider.render();
+
+  const modulePageSlide = new MainSlider({
+    btns: '.next', 
+    container: '.moduleapp', 
+    next: '.nextmodule', 
+    prev: '.prevmodule'
+  });
+  modulePageSlide.render();
 
   const showUpSlider = new MiniSlider({
     container: '.showup__content-slider', 
@@ -33,7 +43,13 @@ window.addEventListener('DOMContentLoaded', () => {
   });
   feedSlider.init();
 
-  const player = new VideoPlayer('.showup__video .play', '.overlay');
-  player.init();
+  new VideoPlayer('.showup .play', '.overlay').init();
+  new VideoPlayer('.video .play', '.overlay').init();
+
+  new VideoPlayer('.module__video-item .play', '.overlay').init();
+
+  new Difference('.officerold', '.officernew', '.officer__card-item').init();
+  
+  new Form('.form').init();
 
 });
--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -3,20 +3,35 @@ export default class VideoPlayer {
     this.buttons = document.querySelectorAll(triggers);
     this.overlay = document.querySelector(overlay);
     this.close = this.overlay.querySelector('.close');
+    this.onPlayerStateChange = this.onPlayerStateChange.bind(this);
   }
 
   bindTriggers() {
-    this.buttons.forEach(btn => {
-      btn.addEventListener('click', () => {
-        if(document.querySelector('iframe#iframe')) {
-          this.overlay.style.display = 'flex';
-        } else {
-          const path = btn.getAttribute('data-url');
+    this.buttons.forEach((btn, i) => {
+      try {
+        const blockedElem = btn.closest('.module__video-item').nextElementSibling;
 
-          this.createPlayer(path);
-
+        if (i % 2 === 0) {
+          blockedElem.setAttribute('data-disabled', 'true');
         }
-        
+      } catch (e) {}
+
+      btn.addEventListener('click', () => {
+        if (!btn.closest('.module__video-item') || btn.closest('.module__video-item').getAttribute('data-disabled') !== 'true') {
+          this.activeBtn = btn;
+          if(document.querySelector('iframe#frame')) {
+            this.overlay.style.display = 'flex';
+  
+            if(this.path !== btn.getAttribute('data-url')) {
+              this.path = btn.getAttribute('data-url');
+              this.player.loadVideoById({videoId: this.path});
+            }
+          } else {
+            this.path = btn.getAttribute('data-url');
+  
+            this.createPlayer(this.path);
+          }
+        }
       });
     });
   }
@@ -32,21 +47,58 @@ export default class VideoPlayer {
     this.player = new YT.Player('frame', {
       height: '100%',
       width: '100%',
-      videoId: `${url}`
+      videoId: `${url}`,
+      events: {
+        //onStateChange будет выполнена при активации события
+        'onStateChange': this.onPlayerStateChange
+      }
     });
-    console.log(this.player);
     this.overlay.style.display = 'flex';
   }
 
+  onPlayerStateChange(state) {
+    try {
+          //найдем ближайший к нашему видео идентичный элемент
+    const blockedElem = this.activeBtn.closest('.module__video-item').nextElementSibling;
+    //глубокое клонирование параметров кнопки от предыдущего видео
+    const playBtn = this.activeBtn.querySelector('svg').cloneNode(true);
+
+    //Если свойства объекта state.data = 0 (воспроизведение видео завершено)
+    if (state.data === 0) {
+      //Если кнопка не активна
+      if (blockedElem.querySelector('.play__circle').classList.contains('closed')) {
+        //То разблокируем видео
+        blockedElem.querySelector('.play__circle').classList.remove('closed');
+        //Удалим значок замочка
+        blockedElem.querySelector('svg').remove();
+        //Назначим кнопке новые параметры
+        blockedElem.querySelector('.play__circle').appendChild(playBtn);
+        //Меняем текст рядом с кнопкой
+        blockedElem.querySelector('.play__text').textContent = 'play video';
+        //Удаляем ненужный класс с текста и меняем стили на видео
+        blockedElem.querySelector('.play__text').classList.remove('attention');
+        blockedElem.style.opacity = 1;
+        blockedElem.style.filter = 'none';
+
+        blockedElem.setAttribute('data-disabled', 'false');
+      }      
+    }
+
+    } catch(e) {}
+
+  }
+
   init() {
-    const tag = document.createElement('script');
+    //Check if there is at least one element in 'buttons'
+    if(this.buttons.length > 0) {
+      const tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
-    const firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    this.bindTriggers();
-    this.bindCloseBtn();
-
+      this.bindTriggers();
+      this.bindCloseBtn();
+    }
   }
 }
